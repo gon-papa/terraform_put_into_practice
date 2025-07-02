a3b6cc5b85078bf6c041544538f3aaae19daf48d
gon-papa <naoki.0821g@gmail.com>
2025-07-03 00:13:51 +0900

    mainに追加

--- environments/staging/main.tf
+++ environments/staging/main.tf
@@ -1,5 +1,17 @@
+# ============================
+# backendバケット
+# ============================
 module "tfstate_storage" {
   source  = "../../modules/tfstate_storage"
+  env     = var.env
+  project = var.project
+}
+
+# ============================
+# ECSの連携サービス
+# ============================
+module "ecs_infra" {
+  source  = "../../modules/ecs_infra"
   env     = var.env
   project = var.project
 }
